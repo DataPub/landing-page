4a2f80d8e9d2402dfa631f8c9d0496e6a0288da9
Bruno Ferrari <b_ferrari@live.com>
2015-02-02 14:44:10 -0200

    sei la

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -19,7 +19,7 @@ $message = $_POST['message'];
 $to = 'b_ferrari@live.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "DataPub Contact Form:  $name";
 $email_body = "Esta e uma mensagem enviada pela formulario de contato do DataPub.\n\n"."Detalhes:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
-$headers = "From: noreply@datapubapp.com.br\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers = "From: noreply@rhcloud.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";	
 mail($to,$email_subject,$email_body,$headers);
 return true;			
