1c157dbac8717c612f1842598de15b83435f59fa
Bruno Ferrari <b_ferrari@live.com>
2015-02-06 15:03:47 -0200

    1

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,5 +1,6 @@
 <?php
 // Check for empty fields
+/*
 if(empty($_POST['name'])  		||
    empty($_POST['email']) 		||
    empty($_POST['phone']) 		||
@@ -8,7 +9,7 @@ if(empty($_POST['name'])  		||
    {
 	echo "No arguments Provided!";
 	return false;
-   }
+   }*/
 	
 $name = $_POST['name'];
 $email_address = $_POST['email'];
